ad80e79248cf475352cec71e5b03d0b9dd04c66e
Jorge <jltamayocabello@gmail.com>
2026-06-04 19:38:27 -0400

    Se revisa la completa el módulo 6, revisando las clausulas WHERE, ORDER BY, LIMIT, OFFSET y alias AS

--- fundamentos_bd/tienda_online.sql
+++ fundamentos_bd/tienda_online.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE usuarios(
 	id SERIAL PRIMARY KEY,
 	nombre VARCHAR(100) NOT NULL,
@@ -24,9 +25,61 @@ FROM usuarios
 WHERE edad > 25;
 
 SELECT nombre, edad
-FROM usuarios
+From usuarios
 ORDER BY edad DESC;
 
 SELECT *
 FROM usuarios
 LIMIT 2;
+
+UPDATE usuarios
+SET email = 'lis.nuevo@gmail.com',
+	edad = 29,
+	nombre = 'Lissete Torres'
+WHERE id = 3;
+
+UPDATE usuarios
+SET edad = edad + 1;
+
+DELETE FROM usuarios
+WHERE id = 3
+
+CREATE TABLE productos (
+	id SERIAL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	precio NUMERIC(10,2) NOT NULL,
+	categoria VARCHAR(50),
+	stock INTEGER DEFAULT 0
+)
+
+INSERT INTO productos (nombre, precio, categoria, stock) VALUES
+('notebook gamer', 800.000, 'electronica', 15),
+('mouse optico', 20.000, 'electronica', 150),
+('polera', 20.000, 'ropa', 125),
+('xbox', 400.000, 'electronica', 153),
+('pantalon', 40.000, 'ropa', 15),
+('notebook normal', 400.000, 'electronica', 15);
+
+SELECT *
+FROM productos
+WHERE categoria = 'electronica';
+
+SELECT nombre, precio
+FROM productos
+WHERE precio > 300.000
+
+SELECT  nombre, precio
+FROM productos
+ORDER BY precio ASC
+
+SELECT  nombre, precio
+FROM productos
+ORDER BY precio ASC
+LIMIT 3
+OFFSET 3
+
+SELECT 
+	nombre AS product,
+	precio AS costo,
+	stock AS cantidad_en_stock
+FROM productos
