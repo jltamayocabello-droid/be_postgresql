55d1772b426aaa4ffd5546e8243832f3a8411ac3
Jorge <jltamayocabello@gmail.com>
2026-06-04 20:44:35 -0400

    Se revisan operadores lógicos y de comparación

--- fundamentos_bd/tienda_online.sql
+++ fundamentos_bd/tienda_online.sql
@@ -83,3 +83,39 @@ SELECT
 	precio AS costo,
 	stock AS cantidad_en_stock
 FROM productos
+
+
+CREATE TABLE ventas (
+	id SERIAL PRIMARY KEY,
+	producto_id INTEGER NOT NULL,
+	cantidad INTEGER NOT NULL,
+	fecha DATE DEFAULT CURRENT_DATE,
+	total NUMERIC(10,2) NOT NULL
+
+INSERT INTO ventas (producto_id, cantidad, fecha, total) VALUES
+(1, 2, '2026-01-20', 820.000),
+(2, 2, '2026-02-20', 320.000),
+(3, 2, '2026-03-03', 345.000),
+(1, 7, '2026-01-20', 820.000),
+(2, 6, '2026-02-20', 120.000),
+(3, 3, '2026-03-03', 35.000)
+
+SELECT * FROM ventas
+
+SELECT nombre, precio, categoria
+FROM productos
+WHERE precio >= 200.000
+	AND categoria = 'electronica'
+	OR nombre LIKE '%gamer%'
+
+SELECT nombre, precio, categoria
+FROM productos
+WHERE precio BETWEEN 300.000 AND 100.000
+
+SELECT nombre, precio, categoria
+FROM productos
+WHERE Categoria IN ('electronica')
+
+SELECT nombre
+FROM productos
+WHERE nombre ILIKE 'a%'
