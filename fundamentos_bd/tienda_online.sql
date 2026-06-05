a1530ceaeaebb760382069007af287aefb547b50
Jorge <jltamayocabello@gmail.com>
2026-06-05 13:30:50 -0400

    Se revisan las funciones agregadas

--- fundamentos_bd/tienda_online.sql
+++ fundamentos_bd/tienda_online.sql
@@ -1,4 +1,5 @@
 
+
 CREATE TABLE usuarios(
 	id SERIAL PRIMARY KEY,
 	nombre VARCHAR(100) NOT NULL,
@@ -53,69 +54,29 @@ CREATE TABLE productos (
 )
 
 INSERT INTO productos (nombre, precio, categoria, stock) VALUES
-('notebook gamer', 800.000, 'electronica', 15),
-('mouse optico', 20.000, 'electronica', 150),
-('polera', 20.000, 'ropa', 125),
-('xbox', 400.000, 'electronica', 153),
-('pantalon', 40.000, 'ropa', 15),
-('notebook normal', 400.000, 'electronica', 15);
+('notebook gamer', 800.000, 'electronica', 15)
+('mouse', 20.000, 'electronica', 150)
+('polera', 20.000, 'ropa', 125)
+('xbox', 400.000, 'electronica', 153)
+('not', 800.000, 'electronica', 15)
+('notebook gamer', 800.000, 'electronica', 15)
 
-SELECT *
-FROM productos
-WHERE categoria = 'electronica';
-
-SELECT nombre, precio
-FROM productos
-WHERE precio > 300.000
-
-SELECT  nombre, precio
-FROM productos
-ORDER BY precio ASC
-
-SELECT  nombre, precio
-FROM productos
-ORDER BY precio ASC
-LIMIT 3
-OFFSET 3
-
-SELECT 
-	nombre AS product,
-	precio AS costo,
-	stock AS cantidad_en_stock
+SELECT COUNT(*) AS total_productos
 FROM productos
 
-
-CREATE TABLE ventas (
-	id SERIAL PRIMARY KEY,
-	producto_id INTEGER NOT NULL,
-	cantidad INTEGER NOT NULL,
-	fecha DATE DEFAULT CURRENT_DATE,
-	total NUMERIC(10,2) NOT NULL
-
-INSERT INTO ventas (producto_id, cantidad, fecha, total) VALUES
-(1, 2, '2026-01-20', 820.000),
-(2, 2, '2026-02-20', 320.000),
-(3, 2, '2026-03-03', 345.000),
-(1, 7, '2026-01-20', 820.000),
-(2, 6, '2026-02-20', 120.000),
-(3, 3, '2026-03-03', 35.000)
+SELECT COUNT(*) AS total_productos
+FROM productos
+WHERE categoria = 'ropa'
 
 SELECT * FROM ventas
 
-SELECT nombre, precio, categoria
-FROM productos
-WHERE precio >= 200.000
-	AND categoria = 'electronica'
-	OR nombre LIKE '%gamer%'
+SELECT SUM(total) AS ingresos_totales
+FROM ventas
 
-SELECT nombre, precio, categoria
+SELECT AVG(precio) AS precio_promedio
 FROM productos
-WHERE precio BETWEEN 300.000 AND 100.000
 
-SELECT nombre, precio, categoria
+SELECT 
+	MIN(precio) AS precio_mas_bajo,
+	MAX(precio) AS precio_mas_alto
 FROM productos
-WHERE Categoria IN ('electronica')
-
-SELECT nombre
-FROM productos
-WHERE nombre ILIKE 'a%'
