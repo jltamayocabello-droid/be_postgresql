0497cb672ce833b4287e1a2b9499dd7a6bd3d601
Jorge <jltamayocabello@gmail.com>
2026-06-10 20:45:37 -0400

    Se crea trigger

--- fundamentos_bd/trigger.sql
+++ fundamentos_bd/trigger.sql
@@ -4,3 +4,31 @@ CREATE TABLE productos (
 	precio NUMERIC
 )
 
+CREATE OR REPLACE FUNCTION validar_precio()
+RETURNS TRIGGER
+AS $$
+BEGIN
+	IF NEW.precio < 0 THEN
+		RAISE EXCEPTION 'El precio no puede ser negativo';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_validar_precio
+BEFORE INSERT
+ON productos
+FOR EACH ROW
+EXECUTE FUNCTION validar_precio();
+
+INSERT INTO productos(nombre, precio)
+VALUES ('Laptp Gamer', 1200)
+
+INSERT INTO productos(nombre, precio)
+VALUES ('Iphone', -1200)
+
+SELECT * FROM productos
+
+
+
